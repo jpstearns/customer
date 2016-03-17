0e2564110724237d30c2ca466d594b14b26404eb
jpstearns <jared.patrick.stearns@gmail.com>
2016-03-17 13:46:49 -0400

    initial commit configuring dropwizard

---
+++ customer-app/src/main/java/com/kindinc/customers/App.java
@@ -0,0 +1,28 @@
+package com.kindinc.customers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application<Configuration> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	@Override
+	public void initialize(Bootstrap<Configuration> b){}
+	@Override
+	public void run (Configuration c, Environment e) throws 
+		Exception{
+			Logger.info("Method App#run() called");
+			System.out.println("Hello World: brought to you by Dropwizard!");
+		}
+    public static void main( String[] args ) throws Exception {
+        new App().run(args);
+    }
+}
